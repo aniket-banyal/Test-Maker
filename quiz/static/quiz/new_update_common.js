1399ee926b08b08d4561810dc62992cbd4cd0ec8
aniket <dummyani26@gmail.com>
2021-03-25 20:42:54 +0530

    Add new option New option can be added by clicking on add option input or its radio input or by input on add option input

--- quiz/static/quiz/new_update_common.js
+++ quiz/static/quiz/new_update_common.js
@@ -1,11 +1,13 @@
-saveBtn = document.querySelector('#save')
-addBtn = document.querySelector('#add')
-submitBtn = document.querySelector('#submit')
-form = document.querySelector('form')
+const saveBtn = document.querySelector('#save')
+const addBtn = document.querySelector('#add')
+const submitBtn = document.querySelector('#submit')
+const form = document.querySelector('form')
+
+let q_number = document.querySelectorAll('.quiz-form__quiz').length + 1
 
 //to prevent form from submitting when enter is pressed in settings modal
 form.addEventListener('keydown', (e) => {
-    if (e.keyCode == 13) {
+    if (e.key == 'Enter') {
         if (e.target.nodeName == 'INPUT') {
             e.preventDefault()
             return false
@@ -13,25 +15,20 @@ form.addEventListener('keydown', (e) => {
     }
 })
 
-let q_number = document.querySelectorAll('.quiz-form__quiz').length + 1
+saveBtn.onclick = () => { submitBtn.click() }
 
-saveBtn.onclick = () => {
-    submitBtn.click()
-}
+addBtn.onclick = () => { createQuestion() }
 
-addBtn.onclick = () => {
-    createQuestion()
-}
 
-function createQuestion(q_inp_value = null, r_inps_checked = null, o_inps_value = null, points_inp_value = 1) {
-    question = document.createElement('div')
+function createQuestion(q_inp_value = null, r_inps_checked = null, o_inps_value = null, points_inp_value = 1, number_of_choices = 4) {
+    const question = document.createElement('div')
     question.classList.add("quiz-form__quiz")
 
-    label = document.createElement('label')
+    const label = document.createElement('label')
     label.setAttribute('for', `q_${q_number}`)
     label.innerHTML = `${q_number}.`
 
-    q_inp = document.createElement('input')
+    const q_inp = document.createElement('input')
     q_inp.type = 'text'
     q_inp.name = `q_${q_number}`
     q_inp.id = `q_${q_number}`
@@ -42,7 +39,7 @@ function createQuestion(q_inp_value = null, r_inps_checked = null, o_inps_value 
     question.appendChild(label)
     question.appendChild(q_inp)
 
-    for (let i = 1; i < 4 + 1; i++) {
+    for (let i = 1; i < number_of_choices + 1; i++) {
         div = document.createElement('div')
         div.classList.add('quiz-form__ans')
 
@@ -69,13 +66,15 @@ function createQuestion(q_inp_value = null, r_inps_checked = null, o_inps_value 
         question.appendChild(div)
     }
 
-    footer = document.createElement('div')
+    question.appendChild(createAddChoiceDiv())
+
+    const footer = document.createElement('div')
     footer.classList.add('footer')
 
-    point = document.createElement('div')
+    const point = document.createElement('div')
     point.classList.add('point')
 
-    points_inp = document.createElement('input')
+    const points_inp = document.createElement('input')
     points_inp.type = 'number'
     points_inp.name = `${q_number}_point`
     points_inp.value = points_inp_value
@@ -91,13 +90,13 @@ function createQuestion(q_inp_value = null, r_inps_checked = null, o_inps_value 
     div = document.createElement('div')
     div.classList.add('footerBtns')
 
-    duplicateBtn = document.createElement('button')
+    const duplicateBtn = document.createElement('button')
     duplicateBtn.type = 'button'
     duplicateBtn.classList.add('duplicateBtn')
     duplicateBtn.innerHTML = 'Duplicate'
     duplicateBtn.addEventListener('click', duplicateQuestion)
 
-    deleteBtn = document.createElement('button')
+    const deleteBtn = document.createElement('button')
     deleteBtn.type = 'button'
     deleteBtn.classList.add('deleteBtn')
     deleteBtn.innerHTML = 'Delete'
@@ -117,6 +116,72 @@ function createQuestion(q_inp_value = null, r_inps_checked = null, o_inps_value 
     question.scrollIntoView()
 }
 
+//add new choice
+const add_choice_text_inps = document.querySelectorAll('.add_choice')
+add_choice_text_inps.forEach(add_choice_text_inp => {
+    add_choice_text_inp.addEventListener('click', addOptionClickHandler)
+    add_choice_text_inp.addEventListener('input', addOptionClickHandler)
+
+    add_choice_radio_inp = add_choice_text_inp.parentElement.querySelector('input[type="radio"]')
+    add_choice_radio_inp.addEventListener('click', addOptionClickHandler)
+})
+
+function addOptionClickHandler(e) {
+    if (e.target.type == 'text') {
+        add_choice_text_inp = e.target
+        parentAnswer = add_choice_text_inp.parentElement
+        add_choice_radio_inp = parentAnswer.querySelector('input[type="radio"]')
+    }
+    else {
+        add_choice_radio_inp = e.target
+        parentAnswer = add_choice_radio_inp.parentElement
+        add_choice_text_inp = parentAnswer.querySelector('input[type="text"]')
+    }
+
+    const parentQuestion = parentAnswer.parentElement
+    const t_q_number = parseInt(parentQuestion.querySelector('label').innerText)
+
+    const i = parentQuestion.querySelectorAll('.quiz-form__ans').length
+
+    add_choice_text_inp.name = `${t_q_number}_option${i}`
+    add_choice_text_inp.required = true
+    add_choice_text_inp.placeholder = `Option ${i}`
+    add_choice_text_inp.classList.remove('add_choice')
+
+    add_choice_radio_inp.name = `${t_q_number}_radio_option`
+    add_choice_radio_inp.value = i
+    add_choice_radio_inp.required = true
+
+    parentAnswer.insertAdjacentElement('afterend', createAddChoiceDiv())
+
+    add_choice_text_inp.removeEventListener('click', addOptionClickHandler)
+    add_choice_text_inp.removeEventListener('input', addOptionClickHandler)
+    add_choice_radio_inp.removeEventListener('click', addOptionClickHandler)
+}
+
+function createAddChoiceDiv() {
+    const div = document.createElement('div')
+    div.classList.add('quiz-form__ans')
+
+    const r_inp = document.createElement('input')
+    r_inp.type = 'radio'
+    r_inp.addEventListener('click', addOptionClickHandler)
+
+    const span = document.createElement('span')
+    span.classList.add('design')
+
+    const o_inp = document.createElement('input')
+    o_inp.type = 'text'
+    o_inp.placeholder = 'Add Option'
+    o_inp.classList.add('add_choice')
+    o_inp.addEventListener('click', addOptionClickHandler)
+    o_inp.addEventListener('input', addOptionClickHandler)
+
+    div.appendChild(r_inp)
+    div.appendChild(span)
+    div.appendChild(o_inp)
+    return div
+}
 
 //duplicate question
 duplicateBtns = document.querySelectorAll('.duplicateBtn')
@@ -134,19 +199,21 @@ function duplicateQuestion(e) {
     for (o_inp of parentQuestion.querySelectorAll('.quiz-form__ans input[type="text"]'))
         o_inps_value.push(o_inp.value)
 
-    points_inp_value = parentQuestion.querySelector('.footer input[type="number"]').value
-
-    createQuestion(q_inp_value, r_inps_checked, o_inps_value, points_inp_value)
+    const points_inp_value = parentQuestion.querySelector('.footer input[type="number"]').value
+    const number_of_choices = parentQuestion.querySelectorAll('.quiz-form__ans input[type="radio"]').length - 1
+    createQuestion(q_inp_value, r_inps_checked, o_inps_value, points_inp_value, number_of_choices)
 }
 
+
 //delete question
-deleteBtns = document.querySelectorAll('.deleteBtn')
+const deleteBtns = document.querySelectorAll('.deleteBtn')
 deleteBtns.forEach(deleteBtn => deleteBtn.addEventListener('click', deleteQuestion))
 
 function deleteQuestion(e) {
     const parentQuestion = e.target.parentElement.parentElement.parentElement
     parentQuestion.remove()
 
+    //dont make this let cuz we wanna acess global q_number
     q_number = 1
     questions = document.querySelectorAll('.quiz-form__quiz')
     for (question of questions) {
@@ -162,7 +229,7 @@ function deleteQuestion(e) {
             r_inp.name = `${q_number}_radio_option`
         }
 
-        i = 1
+        let i = 1
         for (o_inp of question.querySelectorAll('.quiz-form__ans input[type="text"]')) {
             o_inp.name = `${q_number}_option${i}`
             i++
@@ -193,13 +260,12 @@ timerEnabled.addEventListener('change', e => {
     if (e.target.checked) {
         timerDiv.style.display = 'flex'
         timerDurationInp.focus()
-        saveSettingsBtn.disabled = true
+        validate()
     }
     else {
         timerDiv.style.display = 'none'
         saveSettingsBtn.disabled = false
     }
-    validate()
 })
 
 //only enable save button when all inputs have some value
@@ -234,15 +300,17 @@ saveSettingsBtn.addEventListener('click', () => {
     timerStartTime = timerStartTimeInp.value
 })
 
+
 //not allowing to type - on numpad, - on number row, e and +
 timerDurationInp.onkeydown = e => {
     if (e.keyCode == 109 || e.keyCode == 189 || e.keyCode == 69 || e.keyCode == 107 || e.keyCode == 187) return false
 }
 
+
 //allow only positive numbers to be pasted
 timerDurationInp.addEventListener('paste', e => {
     e.preventDefault()
     //regex to replace all characters expect number
-    let paste = parseInt(e.clipboardData.getData('text').replace(/[^\d]/g, ''))
+    const paste = parseInt(e.clipboardData.getData('text').replace(/[^\d]/g, ''))
     e.target.value = Math.abs(paste)
 })
