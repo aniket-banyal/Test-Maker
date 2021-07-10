807d22cf909cfe0fa0c4eaafad5c7abf5b602a78
aniket <dummyani26@gmail.com>
2021-07-10 15:52:11 +0530

    Add django-heroku

--- test_maker/settings.py
+++ test_maker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'quiz-list'
 
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
